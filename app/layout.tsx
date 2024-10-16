85fe44ca97b995e05473f58c66551e5b8dd077cd
pasqualevitiello <pasqualevitiello@gmail.com>
2024-10-16 11:30:27 +0200

    Temporary restore previous version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "@/demo/theme-provider";
 import type { Viewport } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -20,17 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${fontSans.variable} bg-background font-sans antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+    <html lang="en">
+      <body className={`${fontSans.variable} bg-background font-sans antialiased`}>{children}</body>
     </html>
   );
 }
