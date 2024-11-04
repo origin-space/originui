631ac3bb17f35976df2e5ad49818aabb3ce78da2
pasqualevitiello <pasqualevitiello@gmail.com>
2024-11-04 10:01:22 +0100

    mc

--- components/textareas/textarea-11.tsx
+++ components/textareas/textarea-11.tsx
@@ -5,7 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 export default function Textarea11() {
   return (
     <div className="space-y-2">
-      <Label htmlFor="textarea-11">Textarea with end button</Label>
+      <Label htmlFor="textarea-11">Textarea with right button</Label>
       <Textarea id="textarea-11" placeholder="Leave a comment" />
       <div className="flex justify-end">
         <Button variant="outline">Send</Button>
