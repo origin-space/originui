76e1d806a1a71df56babd3d526e95e6f98d1b3a2
pasqualevitiello <pasqualevitiello@gmail.com>
2024-11-20 11:21:25 +0100

    docs: update input title to include texatarea

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Page() {
                       href="/inputs"
                       className="inline-flex w-full items-center justify-between whitespace-nowrap rounded-lg border border-border bg-background p-4 font-bold shadow-sm shadow-black/5 ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-14"
                     >
-                      <span className="truncate">Input</span>
+                      <span className="truncate">Input and Textarea</span>
                       <ArrowRight
                         size={16}
                         strokeWidth={2}
