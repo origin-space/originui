b378e81fd22bd5cf88e4488ca63fafb8707ecc9a
Sinan <muhammedsinan7749@gmail.com>
2024-11-22 20:24:21 +0530

    number removed from name

--- components/buttons/button-53.tsx
+++ components/buttons/button-53.tsx
@@ -6,7 +6,7 @@ import { useTheme } from 'next-themes'
 import { Button } from "@/components/ui/button"
 import { Moon, Sun } from 'lucide-react'
 
-export default function Button53() {
+export default function ButtonDemo() {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark')
