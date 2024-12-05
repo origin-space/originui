3a70e61c54afca1d7970630b0ce78d960828a035
pasqualevitiello <pasqualevitiello@gmail.com>
2024-12-05 19:59:26 +0100

    chore: hover card fix

--- components/ui/hover-card.tsx
+++ components/ui/hover-card.tsx
@@ -1,18 +1,12 @@
 "use client";
-"use client";
 
-import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
-import * as React from "react";
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { cn } from "@/lib/utils";
 
 const HoverCard = HoverCardPrimitive.Root;
-const HoverCard = HoverCardPrimitive.Root;
 
-const HoverCardTrigger = HoverCardPrimitive.Trigger;
 const HoverCardTrigger = HoverCardPrimitive.Trigger;
 
 const HoverCardContent = React.forwardRef<
@@ -39,7 +33,5 @@ const HoverCardContent = React.forwardRef<
   </HoverCardPrimitive.Content>
 ));
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
-
-export { HoverCard, HoverCardContent, HoverCardTrigger };
 
 export { HoverCard, HoverCardContent, HoverCardTrigger };
