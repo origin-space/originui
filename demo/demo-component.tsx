968f848b4e6735337dd19cc280ab2c7f6958d491
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-12-31 18:44:13 +0530

    feat: add CliButton component for copying CLI commands and update DemoComponent to include it

--- demo/demo-component.tsx
+++ demo/demo-component.tsx
@@ -1,4 +1,6 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
+import CliButton from "./cli-button";
 import CopyButton from "./copy-button";
 import { readComponentSource } from "./read-component-source";
 
@@ -6,11 +8,13 @@ export default async function DemoComponent({
   directory,
   componentName,
   className,
+  withCli,
   ...props
 }: {
   directory: string;
   componentName: string;
   className?: string;
+  withCli?: boolean;
   [key: string]: any;
 }) {
   const Component = (await import(`@/components/${directory}/${componentName}`)).default;
@@ -19,7 +23,12 @@ export default async function DemoComponent({
   return (
     <div className={cn("group/item relative", className)}>
       <Component {...props} />
-      <CopyButton componentSource={source || ""} />
+      <div className="absolute right-2 top-2 flex items-center transition-opacity lg:opacity-0 lg:group-focus-within/item:opacity-100 lg:group-hover/item:opacity-100">
+        <TooltipProvider delayDuration={0}>
+          <CopyButton componentSource={source || ""} />
+          {withCli && <CliButton componentName={componentName} />}
+        </TooltipProvider>
+      </div>
     </div>
   );
 }
