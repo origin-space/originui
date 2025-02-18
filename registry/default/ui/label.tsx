20d76892ed206180a3c54f60bee04c1418a828f8
pasqualevitiello <pasqualevitiello@gmail.com>
2025-02-18 17:19:47 +0100

    chore: convert label, pagination

--- registry/default/ui/label.tsx
+++ registry/default/ui/label.tsx
@@ -1,22 +1,24 @@
 "use client";
 
 import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label"
 
 import { cn } from "@/registry/default/lib/utils";
 
-const Label = React.forwardRef<HTMLLabelElement, React.LabelHTMLAttributes<HTMLLabelElement>>(
-  ({ className, ...props }, ref) => (
-    <label
-      ref={ref}
+function Label({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        "text-foreground text-sm leading-4 font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        "text-foreground text-sm leading-4 font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className,
       )}
       {...props}
     />
-  ),
-);
-Label.displayName = "Label";
+  )
+}
 
-export { Label };
+export { Label }
