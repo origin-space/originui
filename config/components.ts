6e982a5d6b9c1923314d54e72c0626b6b18ad4b7
pasqualevitiello <pasqualevitiello@gmail.com>
2025-02-24 18:36:39 +0100

    feat: add one more timeline

--- config/components.ts
+++ config/components.ts
@@ -671,6 +671,7 @@ export const categories: ComponentCategory[] = [
       { name: "comp-534" },
       { name: "comp-535" },
       { name: "comp-536" },
+      { name: "comp-537" },
       { name: "comp-531" },
       { name: "comp-533" },
     ],
