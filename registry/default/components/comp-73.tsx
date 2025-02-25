4d1af6753373ace3d84dc8a5e51fddec34c2e91d
pasqualevitiello <pasqualevitiello@gmail.com>
2025-02-25 15:04:10 +0100

    fix: input with label animation on Safari

--- registry/default/components/comp-73.tsx
+++ registry/default/components/comp-73.tsx
@@ -3,7 +3,7 @@ import { useId } from "react";
 export default function Component() {
   const id = useId();
   return (
-    <div className="border-input bg-background focus-within:border-ring focus-within:ring-ring/50 has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 has-aria-invalid:border-destructive relative rounded-md border shadow-xs transition-[color,box-shadow] outline-none focus-within:ring-[3px] has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 has-[input:is(:disabled)_*]:pointer-events-none">
+    <div className="border-input bg-background focus-within:border-ring focus-within:ring-ring/50 has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 has-aria-invalid:border-destructive relative rounded-md border shadow-xs transition-[color,box-shadow] outline-none focus-within:ring-[3px] has-disabled:pointer-events-none has-disabled:cursor-not-allowed has-disabled:opacity-50 has-[input:is(:disabled)]:*:pointer-events-none">
       <label htmlFor={id} className="text-foreground block px-3 pt-2 text-xs font-medium">
         Textarea with inset label
       </label>
