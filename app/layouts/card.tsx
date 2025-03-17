82368acdb5b6d1bf3f72ef1bcbda9c2caccada8e
pasqualevitiello <pasqualevitiello@gmail.com>
2025-03-17 12:48:28 +0100

    chore: open layouts in a new tab

--- app/layouts/card.tsx
+++ app/layouts/card.tsx
@@ -20,7 +20,7 @@ export default function Card({ card }: CardProps) {
         <h2 className="text-sm font-medium">{card.title}</h2>
         {card.demoUrl && card.repoUrl ? (
           <div className="flex items-center gap-6">
-            <Link href={card.demoUrl} className="inline-flex items-center gap-1 text-sm hover:underline">
+            <Link href={card.demoUrl} className="inline-flex items-center gap-1 text-sm hover:underline" target="_blank">
               Live Demo
               <span className="text-muted-foreground">
                 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" aria-hidden="true"><path d="M6.37 6.486 6.356 1.11H1.008L2.058.06H7.42v5.376l-1.05 1.05ZM.336 6.374 6.538.172l.77.77-6.202 6.202-.77-.77Z" /></svg>
