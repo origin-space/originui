d26946d5252b22e66c1bdb2f35ee49110dd72392
pasqualevitiello <pasqualevitiello@gmail.com>
2025-03-27 13:35:30 +0100

    fix: error

--- registry/default/components/comp-542.tsx
+++ registry/default/components/comp-542.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { addDays, setHours, setMinutes } from "date-fns"
+import { addDays, subDays, setHours, setMinutes } from "date-fns"
 
 import {
   EventCalendar,
