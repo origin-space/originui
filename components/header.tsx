ff1f818d07779dd3f80624cd4155f2c6b5662cf4
Neeraj Dalal <admin@nrjdalal.com>
2025-04-25 17:07:21 +0530

    fix: navbar initial load flicker > header.tsx
    components/header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,6 @@ import LogoDark from "@/public/logo-dark.svg"
 import Logo from "@/public/logo.svg"
 import { RiGithubFill, RiMenu2Line, RiTwitterXFill } from "@remixicon/react"
 
-import { useIsMobile } from "@/hooks/use-mobile"
 import HeaderLink from "@/components/header-link"
 import ThemeToggle from "@/components/theme-toggle"
 import {
@@ -23,8 +22,6 @@ const links = [
 ]
 
 export default function Header() {
-  const isMobile = useIsMobile()
-
   return (
     <header className="before:bg-[linear-gradient(to_right,--theme(--color-border/.3),--theme(--color-border)_200px,--theme(--color-border)_calc(100%-200px),--theme(--color-border/.3))] relative mb-14 before:absolute before:-inset-x-32 before:bottom-0 before:h-px">
       <div
@@ -52,24 +49,23 @@ export default function Header() {
           />
         </Link>
         <div className="flex items-center">
-          {!isMobile && (
-            <>
-              <div className="flex items-center gap-4 md:gap-10">
-                {links.map((link) => (
-                  <HeaderLink
-                    key={link.href}
-                    text={link.text}
-                    href={link.href}
-                    isNew={link.isNew}
-                  />
-                ))}
-              </div>
-              <div
-                className="bg-input ms-4 me-4 h-5 w-px md:ms-10"
-                aria-hidden="true"
-              ></div>
-            </>
-          )}
+          <div className="hidden items-center md:flex">
+            <div className="flex items-center gap-4 md:gap-10">
+              {links.map((link) => (
+                <HeaderLink
+                  key={link.href}
+                  text={link.text}
+                  href={link.href}
+                  isNew={link.isNew}
+                />
+              ))}
+            </div>
+            <div
+              className="bg-input ms-4 me-4 h-5 w-px md:ms-10"
+              aria-hidden="true"
+            ></div>
+          </div>
+
           <div className="flex items-center gap-1">
             <a
               className="text-muted-foreground hover:text-foreground/80 focus-visible:border-ring focus-visible:ring-ring/50 inline-flex size-9 items-center justify-center rounded outline-none focus-visible:ring-[3px]"
@@ -90,26 +86,25 @@ export default function Header() {
               <RiGithubFill size={20} />
             </a>
             <ThemeToggle />
-            {isMobile && (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <button className="text-muted-foreground hover:text-foreground/80 focus-visible:border-ring focus-visible:ring-ring/50 inline-flex size-9 items-center justify-center rounded outline-none focus-visible:ring-[3px]">
-                    <RiMenu2Line className="size-5" size={20} />
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  {links.map((link) => (
-                    <DropdownMenuItem
-                      className="cursor-pointer focus:bg-transparent focus:underline"
-                      key={link.href}
-                      asChild
-                    >
-                      <HeaderLink text={link.text} href={link.href} />
-                    </DropdownMenuItem>
-                  ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            )}
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="text-muted-foreground hover:text-foreground/80 focus-visible:border-ring focus-visible:ring-ring/50 inline-flex size-9 items-center justify-center rounded outline-none focus-visible:ring-[3px] md:hidden">
+                  <RiMenu2Line className="size-5" size={20} />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                {links.map((link) => (
+                  <DropdownMenuItem
+                    className="cursor-pointer focus:bg-transparent focus:underline"
+                    key={link.href}
+                    asChild
+                  >
+                    <HeaderLink text={link.text} href={link.href} />
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </div>
